bea07c873d9d5d95ad6226b5d811af7a7d412b28
wenoptics <wenoptics@163.com>
2018-02-01 18:20:08 +0800

    working find moon

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 import cv2
-import numpy as np
 
-ori_img = cv2.imread('test_dataset/DSC01332.jpg')
-ori_img = cv2.resize(ori_img, (0, 0), fx=0.2, fy=0.2)
-gray_img = cv2.cvtColor(ori_img, cv2.COLOR_BGR2GRAY)
-img = cv2.medianBlur(gray_img, 5)
-cimg = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+from tryFindCircle import find_circle
 
-circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 120,
-                           param1=100, param2=30, minRadius=0, maxRadius=0)
+test_img = [
+    'test_dataset/DSC01313.jpg',
+    'test_dataset/DSC01332.jpg',
+    'test_dataset/DSC01422.jpg',
+    'test_dataset/DSC01426.jpg',
+]
 
-circles = np.uint16(np.around(circles))
+for img_path in test_img:
+    ori_img = cv2.imread(img_path)
+    img = cv2.resize(ori_img, (0, 0), fx=0.05, fy=0.05)
 
-for i in circles[0, :]:
-    # draw the outer circle
-    cv2.circle(ori_img, (i[0], i[1]), i[2], (0, 255, 0), 2)
-    # draw the center of the circle
-    cv2.circle(ori_img, (i[0], i[1]), 2, (0, 0, 255), 3)
+    circles = find_circle(img, show_debug_preview=False)
+    if not circles:
+        print('moon not detected in {}'.format(img_path))
+        cv2.imshow('ori - {}'.format(img_path), img)
+        continue
 
-cv2.imwrite("planets_circles.jpg", ori_img)
-cv2.imshow("HoughCirlces", ori_img)
+    for i in circles:
+        # draw the outer circle
+        cv2.circle(img, (i.x, i.y), i.r, (0, 255, 0), 2)
+        # draw the center of the circle
+        cv2.circle(img, (i.x, i.y), 5, (0, 0, 255), 1)
+
+    cv2.imshow('moondetect - {}'.format(img_path), img)
+    cv2.imwrite('img/moondetect_preview.jpg'.format(img_path), img)
+
 cv2.waitKey()
 cv2.destroyAllWindows()
--- tryFindCircle.py
+++ tryFindCircle.py
@@ -5,20 +5,23 @@ import numpy as np
 
 from utils.util import myTuneWindow
 
-test_img = [
-    'test_dataset/DSC01313.jpg',
-    # 'test_dataset/DSC01332.jpg',
-    # 'test_dataset/DSC01422.jpg',
-    # 'test_dataset/DSC01426.jpg',
-]
 
+class Circle():
+    def __init__(self, x, y, r):
+        self.r = r
+        self.y = y
+        self.x = x
+
+# Normalize image
 dst_width = 1472
-
 
 def scale_to_normal(img):
     h, w, _ = img.shape
+    if w < dst_width:
+        print('[W] input image has size(w={}) smaller than normailize size(w={})'.format(w, dst_width))
+
     factor = dst_width / w
-    return cv2.resize(img, (0, 0), fx=0.2, fy=0.2)
+    return cv2.resize(img, (0, 0), fx=factor, fy=factor), factor
 
 
 def adp_thresh_grayscale(gray, thr=250):
@@ -38,9 +41,12 @@ def find_circle(img,
                 valHoughParam1=172,
                 valHoughParam2=6,
                 valHoughMinDist=900,
+                show_debug_preview=True
                 ):
     target_windowsname = 'final'
     img = img.copy()
+
+    img, factor = scale_to_normal(img)
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # gray_img = adp_thresh_grayscale(gray_img, valAdaptivateThreshold)
@@ -49,15 +55,15 @@ def find_circle(img,
     #   eliminate noise outside the target
     mopen_kernel = np.ones((valKernelOpen, valKernelOpen), np.uint8)
     gray_img = cv2.morphologyEx(gray_img, cv2.MORPH_OPEN, mopen_kernel)
-    cv2.imshow('MORPH_OPEN', gray_img)
+    if show_debug_preview: cv2.imshow('MORPH_OPEN', gray_img)
 
     # Eliminate the noise inside
     mclose_kernel = np.ones((valKernelClose, valKernelClose), np.uint8)
     gray_img = cv2.morphologyEx(gray_img, cv2.MORPH_CLOSE, mclose_kernel)
-    cv2.imshow('MORPH_CLOSE', gray_img)
+    if show_debug_preview: cv2.imshow('MORPH_CLOSE', gray_img)
 
     pimg = cv2.medianBlur(gray_img, valMedianBlur)
-    cv2.imshow('medianBlur', pimg)
+    if show_debug_preview: cv2.imshow('medianBlur', pimg)
     # pimg = cv2.cvtColor(pimg, cv2.COLOR_GRAY2BGR)
 
     circles = cv2.HoughCircles(pimg, cv2.HOUGH_GRADIENT, 1, valHoughMinDist,
@@ -67,16 +73,27 @@ def find_circle(img,
         cv2.destroyWindow(target_windowsname)
         return
 
-    circles = np.uint16(np.around(circles))
+    draw_circles = np.uint16(np.around(circles))
+    ret = []
 
-    for i in circles[0, :]:
+    for i in draw_circles[0, :]:
         # draw the outer circle
         cv2.circle(img, (i[0], i[1]), i[2], (0, 255, 0), 1)
         # draw the center of the circle
         cv2.circle(img, (i[0], i[1]), 2, (0, 0, 255), 1)
 
+    for i in circles[0, :]:
+
+        # Scale the circle back to ori size
+        x = int(i[0] / factor)
+        y = int(i[1] / factor)
+        z = int(i[2] / factor)
+
+        ret.append(Circle(x, y, z))
+
     # cv2.imwrite("1.jpg", img)
-    cv2.imshow(target_windowsname, img)
+    if show_debug_preview: cv2.imshow(target_windowsname, img)
+    return ret
 
 
 def loadimg():
@@ -87,8 +104,8 @@ def loadimg():
         # 'test_dataset/DSC01426.jpg'
     )
     # ori_img = cv2.resize(ori_img, (0, 0), fx=0.2, fy=0.2)
-    ori_img = scale_to_normal(ori_img)
-    print(ori_img.shape)
+    print('origin img.shape={}'.format(ori_img.shape))
+
     myTuneWindow(find_circle, ori_img,
                  valMedianBlur=(1, 30, 2),
                  valKernelOpen=(1, 100, 2),
